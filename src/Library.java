0a9ee3fff37017bf925653215f046530b62eeb4c
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-09-02 20:01:57 -0400

    Add Euclid algorithm

---
+++ src/Library.java
@@ -0,0 +1,10 @@
+public class Library
+{
+    //  Euclid's algorithm
+    public static int gcd(int p, int q)
+    {
+        if (q == 0) return p;
+        int r = p % q;
+        return gcd(q, r);
+    }
+}
